f7243fa2a1f901ede1c7387588f2c988fbb8c4b9
vinnr20 <56141082+vinnr20@users.noreply.github.com>
2019-10-23 13:02:35 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$dbuser = getenv("root");
+$dbpwd = getenv("redhat123");
+$dbname = getenv("testing");
+
+$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+if ($connection->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+} else {
+    printf("Connected to the database");
+}
+$connection->close();
+?>
