7e1d353d7090de67d6df592fd569e11d4d025d5f
vinnr20 <56141082+vinnr20@users.noreply.github.com>
2020-03-05 10:59:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 $dbhost = getenv("MYSQL_SERVICE_HOST");
-$dbport = getenv(MYSQL_SERVICE_PORT);
+$dbport = getenv("MYSQL_SERVICE_PORT");
 $dbuser = getenv("userD5T");
 $dbpwd = getenv("redhat123");
 $dbname = getenv("testing");
 
-$connection = mysqli_connect($dbhost, $dbport, $dbuser, $dbpwd, $dbname);
+$connection = mysql_connect($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
